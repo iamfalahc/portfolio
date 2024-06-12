7f54b92ed1a0a1bea5926cacd83d8fb22580f676
Muhammed Falah <iamfalahc@gmail.com>
2024-06-12 23:03:55 +0530

    profile section completed

--- src/containers/profile/Profile.jsx
+++ src/containers/profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../container.css";
 import { FaGithub } from "react-icons/fa";
 import { IoLogoLinkedin } from "react-icons/io5";
+import profileImage from "../../assets/Picsart_24-01-21_21-01-42-237-01.jpeg"
 const Profile = () => {
   return (
     <div className="profile">
@@ -12,14 +13,17 @@ const Profile = () => {
         </div>
         <h4>MERN STACK DEVELOPER</h4>
         <div className="social-platform">
-          <FaGithub />
-          <IoLogoLinkedin />
+          <FaGithub className="social-icon" />
+          <IoLogoLinkedin className="social-icon" />
         </div>
         <span className="description">
         With a passion for developing modern React web apps for commercial businesses
         </span>
+        <button className="resume-btn">Resume</button>
       </section>
-      <section className="profile-image"></section>
+      <section className="image-section">
+        <img className="profile-image" src={profileImage} alt="profile-image" />
+      </section>
     </div>
   );
 };
